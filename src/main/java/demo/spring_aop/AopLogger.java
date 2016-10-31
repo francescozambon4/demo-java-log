629a0e76fefc9c59b900ac7e23fe322dabad8bfb
francescozambon4 <francesco.zambon.4@gmail.com>
2016-10-31 11:17:25 +0100

    spring aop: add advice around, fix old test

--- src/main/java/demo/spring_aop/AopLogger.java
+++ src/main/java/demo/spring_aop/AopLogger.java
@@ -1,25 +1,35 @@
 package demo.spring_aop;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AopLogger {
-	
-	Logger LOGGER = LoggerFactory.getLogger(AopLogger.class); 
-	
-	public void logBeforeMethod() {
-		LOGGER.info("--- before");
+
+	Logger LOGGER = LoggerFactory.getLogger(AopLogger.class);
+
+	public void logBeforeMethod(JoinPoint joinPoint) {
+		LOGGER.info("--- before, hashcode : " + joinPoint.getTarget().hashCode());
 	}
-	
+
 	public void logAfterReturningMethod(Object retVal) {
-		LOGGER.info("--- return value : " + retVal);
+		LOGGER.info("--- return value : " + (null != retVal ? retVal : "NULL!!!"));
 	}
-	
+
 	public void logAfterThrowingMethod(Exception ex) {
 		LOGGER.error("--- afterThrowing", ex);
 	}
-	
-	public void logAfterMethod() {
-		LOGGER.info("--- after");
+
+	public void logAfterMethod(JoinPoint joinPoint) {
+		LOGGER.info("--- after, hashcode : " + joinPoint.getTarget().hashCode());
+	}
+
+	public Object traceExcetionTime(ProceedingJoinPoint pjp) throws Throwable {
+		long start = System.currentTimeMillis();
+		Object retVal = pjp.proceed();
+		long stop = System.currentTimeMillis();
+		LOGGER.info("--- execution time : " + (stop - start) + "(ms)");
+		return retVal;
 	}
 }
